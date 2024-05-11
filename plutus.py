ba91059b651f9986e0073efad426b0bf3858ef34
psionyx2311 <42845289+psionyx2311@users.noreply.github.com>
2024-05-11 09:51:23 -0400

    Update plutus.py

--- plutus.py
+++ plutus.py
@@ -11,73 +11,15 @@ import binascii
 import os
 import sys
 import time
+from generate_private_key import generate_private_key
+from private_key_to_public_key import private_key_to_public_key
+from public_key_to_address import public_key_to_address
+from private_key_to_wif import private_key_to_wif
+from main import main
+from timer import timer
 
 DATABASE = r'database/11_13_2022/'
 
-def generate_private_key():
-    return binascii.hexlify(os.urandom(32)).decode('utf-8').upper()
-
-def private_key_to_public_key(private_key, fastecdsa):
-    if fastecdsa:
-        key = keys.get_public_key(int('0x' + private_key, 0), curve.secp256k1)
-        return '04' + (hex(key.x)[2:] + hex(key.y)[2:]).zfill(128)
-    else:
-        pk = PrivateKey().fromString(bytes.fromhex(private_key))
-        return '04' + pk.publicKey().toString().hex().upper()
-
-def public_key_to_address(public_key):
-    output = []
-    alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-    var = hashlib.new('ripemd160')
-    encoding = binascii.unhexlify(public_key.encode())
-    var.update(hashlib.sha256(encoding).digest())
-    var_encoded = ('00' + var.hexdigest()).encode()
-    digest = hashlib.sha256(binascii.unhexlify(var_encoded)).digest()
-    var_hex = '00' + var.hexdigest() + hashlib.sha256(digest).hexdigest()[0:8]
-    count = [char != '0' for char in var_hex].index(True) // 2
-    n = int(var_hex, 16)
-    while n > 0:
-        n, remainder = divmod(n, 58)
-        output.append(alphabet[remainder])
-    for i in range(count): output.append(alphabet[0])
-    return ''.join(output[::-1])
-
-def private_key_to_wif(private_key):
-    digest = hashlib.sha256(binascii.unhexlify('80' + private_key)).hexdigest()
-    var = hashlib.sha256(binascii.unhexlify(digest)).hexdigest()
-    var = binascii.unhexlify('80' + private_key + var[0:8])
-    alphabet = chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-    value = pad = 0
-    result = ''
-    for i, c in enumerate(var[::-1]): value += 256**i * c
-    while value >= len(alphabet):
-        div, mod = divmod(value, len(alphabet))
-        result, value = chars[mod] + result, div
-    result = chars[value] + result
-    for c in var:
-        if c == 0: pad += 1
-        else: break
-    return chars[0] * pad + result
-
-def main(database, args):
-    while True:
-        private_key = generate_private_key()
-        public_key = private_key_to_public_key(private_key, args['fastecdsa']) 
-        address = public_key_to_address(public_key)
-
-        if args['verbose']:
-            print(address)
-        
-        if address[-args['substring']:] in database:
-            for filename in os.listdir(DATABASE):
-                with open(DATABASE + filename) as file:
-                    if address in file.read():
-                        with open('plutus.txt', 'a') as plutus:
-                            plutus.write('hex private key: ' + str(private_key) + '\n' +
-                                         'WIF private key: ' + str(private_key_to_wif(private_key)) + '\n'
-                                         'public key: ' + str(public_key) + '\n' +
-                                         'uncompressed address: ' + str(address) + '\n\n')
-                        break
 
 def print_help():
     print('''Plutus homepage: https://github.com/Isaacdelly/Plutus
@@ -100,14 +42,6 @@ substring: to make the program memory efficient, the entire bitcoin address is n
 cpu_count: number of cores to run concurrently. More cores = more resource usage but faster bruteforcing. Omit this parameter to run with the maximum number of cores''')
     sys.exit(0)
 
-def timer(args):
-    start = time.time()
-    private_key = generate_private_key()
-    public_key = private_key_to_public_key(private_key, args['fastecdsa'])
-    address = public_key_to_address(public_key)
-    end = time.time()
-    print(str(end - start))
-    sys.exit(0)
 
 if __name__ == '__main__':
     args = {
